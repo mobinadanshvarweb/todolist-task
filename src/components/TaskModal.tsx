93bf27e3aa0187830f33242889f2a9a4df506458
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2025-10-28 21:45:31 +0330

    final build

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -29,10 +29,17 @@ const TaskModal = () => {
       return;
     }
 
+    const now = new Date();
+
     const newTask = {
       ...taskData,
       id: Date.now().toString(),
-      createdAt: new Date().toISOString(),
+      createdAt: `${now.getFullYear()}-${(now.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")} ${now
+        .getHours()
+        .toString()
+        .padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}`,
     };
     dispatch(addTask(newTask));
     setTaskData({ title: "", description: "" });
@@ -44,7 +51,7 @@ const TaskModal = () => {
 
   return (
     <div className="w-full h-screen fixed top-0 left-0 bg-black/50 flex justify-center items-center z-10">
-      <div className="w-[70%] min-h-64 rounded bg-background flex flex-col gap-4 p-6">
+      <div className="w-[50%] min-h-64 rounded bg-background flex flex-col gap-4 p-6">
         <h2 className="text-xl font-bold">Add New Task</h2>
         <div className="flex flex-col gap-4 flex-1">
           <input
@@ -72,14 +79,14 @@ const TaskModal = () => {
         </div>
         <div className="flex gap-2 justify-end">
           <Button
-            action={handleCancle}
-            style="px-4 py-1 rounded-xl text-white bg-customRed"
-            text="Cancel"
-          />
-          <Button
             action={handleSubmit}
             style="px-4 py-1 rounded-xl text-white bg-customBlue"
             text="Add Task"
+          />
+          <Button
+            action={handleCancle}
+            style="px-4 py-1 rounded-xl text-white bg-customRed"
+            text="Cancel"
           />
         </div>
       </div>
